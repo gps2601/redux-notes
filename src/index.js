5067283489b65ca588ab9cf4c5210ce3c54e9dd4
gps2601 <smith260194@gmail.com>
2019-09-25 07:11:16 +0100

    Splitting into seprate components

--- src/index.js
+++ src/index.js
@@ -3,44 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux'
 import noteReducer from './reducers/noteReducer'
-
+import App from './App'
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const App = () => {
-  return(
-    <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-
 const renderApp = () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(<App store={store}/>, 
+  document.getElementById('root'));
 }
 
 renderApp()
