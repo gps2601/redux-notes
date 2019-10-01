f34df442bd660455d593ca6b79fba032e03f7295
gps2601 <smith260194@gmail.com>
2019-10-01 22:34:41 +0100

    Adding a mock server to fetch and store notes in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import VisbilityFilter from './components/VisibilityFilter'
+import noteService from './services/notes'
+import { initializeNote } from './reducers/noteReducer'
 
 const App = (props) => {
+  useEffect(() => {
+    noteService.getAll().then(notes => props.initializeNote(notes))
+  })
   return(
     <div>
       <NewNote />
@@ -13,4 +19,4 @@ const App = (props) => {
   )
 }
 
-export default App
+export default connect(null, {initializeNote})(App)
